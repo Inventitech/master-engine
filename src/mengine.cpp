e0efb7a43c21ec6ee81ef7a219defd18f270d31f
Thomas Kinnen <thomas.kinnen@gmail.com>
2013-10-16 20:53:16 +0200

    Wobbly triangle :)

--- src/mengine.cpp
+++ src/mengine.cpp
@@ -5,15 +5,19 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "shader.h"
+#include "triangle.h"
 
 using namespace glm;
 
 GLuint program;
-GLint attribute_coord3d;
-GLuint vbo_triangle;
+GLint attribute_coord3d, attribute_v_color;
+GLuint vbo_triangle, vbo_triangle_colors;
 GLFWwindow* window;
+GLint uniform_m_transform;
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 static void error_callback(int error, const char* description) {
@@ -70,6 +74,10 @@ bool initData() {
 	program = LoadShaders("src/shaders/vertexshader.glsl",
 			"src/shaders/fragmentshader.glsl");
 
+	struct attributes triangle_attributes[] = { { { 0.0, 0.8, 0.0 }, { 1.0, 1.0,
+			0.0 } }, { { -0.8, -0.8, 0.0 }, { 0.0, 0.0, 1.0 } }, { { 0.8, -0.8,
+			0.0 }, { 1.0, 0.0, 0.0 } } };
+
 	// Setup vertex shader attribute
 	const char* attribute_name = "coord3d";
 	attribute_coord3d = glGetAttribLocation(program, attribute_name);
@@ -78,12 +86,27 @@ bool initData() {
 		return false;
 	}
 
-	// Triangle data
-	GLfloat triangle_vertices[] = { 0.0, 0.8, 0.0, -0.8, -0.8, 0.0,  0.8, -0.8, 0.0, };
+	attribute_name = "m_transform";
+	uniform_m_transform = glGetUniformLocation(program, attribute_name);
+	if (uniform_m_transform == -1) {
+		fprintf(stderr, "Could not bind uniform %s\n", attribute_name);
+		return 0;
+	}
+
+	// Setup vertex shader attribute
+	attribute_name = "v_color";
+	attribute_v_color = glGetAttribLocation(program, attribute_name);
+	if (attribute_v_color == -1) {
+		fprintf(stderr, "Could not bind attribute %s\n", attribute_name);
+		return false;
+	}
+
 	glGenBuffers(1, &vbo_triangle);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices,
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_attributes),
+			triangle_attributes,
 			GL_STATIC_DRAW);
+
 	return true;
 }
 
@@ -92,24 +115,47 @@ void render() {
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+	double elapsedTime = glfwGetTime();
+	float move = sinf(elapsedTime  * (2 * 3.14) / 5); // -1<->+1 every 5 seconds
+	float angle = elapsedTime  * 45;  // 45° per second
+	glm::vec3 axis_z(0, 0, 1);
+	glm::mat4 m_transform = glm::translate(glm::mat4(1.0f),
+			glm::vec3(move, 0.0, 0.0))
+			* glm::rotate(glm::mat4(1.0f), angle, axis_z);
+	glUniformMatrix4fv(uniform_m_transform, 1, GL_FALSE,
+			glm::value_ptr(m_transform));
+
 	/* Clear the background as white */
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
-
 	glUseProgram(program);
+
+	glEnableVertexAttribArray(attribute_v_color);
 	glEnableVertexAttribArray(attribute_coord3d);
-	/* Describe our vertices array to OpenGL (it can't guess its format automatically) */
 	glBindBuffer(GL_ARRAY_BUFFER, vbo_triangle);
-	glVertexAttribPointer(attribute_coord3d, // attribute
-			3,                 // number of elements per vertex, here (x,y)
-			GL_FLOAT,          // the type of each element
-			GL_FALSE,          // take our values as-is
-			0,                 // offset to first entry
-			0  // pointer to the C array
+
+	/* Describe our colors array to OpenGL (it can't guess its format automatically) */
+	glVertexAttribPointer(attribute_v_color,   // attribute
+			3,                   // number of elements per vertex, here (x,y,z)
+			GL_FLOAT,            // the type of each element
+			GL_FALSE,            // take our values as-is
+			sizeof(struct attributes),  // next coord3d appears every 6 floats
+			(GLvoid*) offsetof(struct attributes, v_color) // offset of first element
+					);
+
+	/* Describe our vertices array to OpenGL (it can't guess its format automatically) */
+	glVertexAttribPointer(attribute_coord3d,   // attribute
+			3,                   // number of elements per vertex, here (x,y,z)
+			GL_FLOAT,            // the type of each element
+			GL_FALSE,            // take our values as-is
+			sizeof(struct attributes),  // next coord3d appears every 6 floats
+			0                    // offset of first element
 			);
 
 	/* Push each element in buffer_vertices to the vertex shader */
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+
+	glDisableVertexAttribArray(attribute_v_color);
 	glDisableVertexAttribArray(attribute_coord3d);
 
 }
@@ -133,7 +179,7 @@ int main(void) {
 		render();
 
 		/* Swap front and back buffers */
-		glfwSwapBuffers (window);
+		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	glfwDestroyWindow(window);
